159290319a4df43934ee1f94e87a0e0f060f8a39
Brianna Iliev <brianna.iliev@sumup.com>
2020-04-17 10:23:29 +0300

    Styles added

--- src/client/index.js
+++ src/client/index.js
@@ -6,4 +6,53 @@ form.addEventListener('submit', handleFormSubmit);
 
 
 
+<<<<<<< Updated upstream
 document.getElementById("submit").addEventListener("click", handleSubmit);
+=======
+const data = await getData(`http://localhost:8080/weather,${lat},${lon}`);
+
+const daily = data.daily.summary;
+console.log(daily);
+const dailyIcon = data.daily.icon;
+console.log(dailyIcon);
+
+
+// const { lat, lon } = await postData('http://localhost:8080/city', { city })
+// const weather = await postData('http://localhost:8080/weather', { lat, lon });
+console.log(data);
+return { city, weather };
+
+}
+
+let allTrips = [];
+
+const handleAdd = () => {}
+
+const handleRemove = () => {}
+
+const createTripDetails = tripDetails => {
+  const container = document.getElementById('container');
+
+  /// create dom -> <button onclick="handleAdd">Add</button> <button>Remove</button>
+
+  container.appendChild() // and append the created dom
+}
+
+// STARTING POINT 
+const button = document.getElementById("submit");
+
+button.addEventListener('click', async e => {
+e.preventDefault();
+
+const city = document.getElementById('city').value;
+const startDate = document.getElementById('start_date').value;
+
+const tripDetails = await getTripDetails(city, startDate);
+
+  // const tripDetails = await getTripDetails(city);
+
+  // createTripDetails(tripDetails)
+
+  // allTrips.push(tripDetails);
+});
+>>>>>>> Stashed changes
--- src/server/server.js
+++ src/server/server.js
@@ -34,12 +34,25 @@ app.post("/city", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+<<<<<<< Updated upstream
   const secretKey = 'a53b81b6e6aaa6c4367dfe55c5001474';
   const url = `https://api.darksky.net/forecast/${secretKey}/40.71427,-74.00597`;
+=======
+  const lat = req.body.lat;
+  const lon = req.body.lon;
+  console.log('This is the lat' + lat);
+  console.log('This is the lon' + lon);
+  weatherData.lat = lat;
+  weatherData.lon = lon;
+  const secretKey = 'a53b81b6e6aaa6c4367dfe55c5001474';
+  const url = 'https://api.darksky.net/forecast/:secretKey/:lat,:lon';
+  console.log(url);
+>>>>>>> Stashed changes
   fetch(url)
   .then(res => res.json())
   .then(data => {
       res.send({ data });
+      console.log(data);
   })
   .catch(err => {
       res.send(err);
